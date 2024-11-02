06d05acd1da3c7778f9e99969ac1fe720c59f933
shriejan <shriej2020@gmail.com>
2024-11-02 18:47:47 +1100

    Structuring folders

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello, World!'
-
+@app.route('/shriejan')
+def index2():
+    return 'Hello, Shriejan!'
 if __name__ == '__main__':
     app.run(debug=True)
